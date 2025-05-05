01ad422b8f6a49dc00bad6c8af6f6bc01e35e62e
Kacperski81 <kacperski81@gmail.com>
2025-05-05 21:31:02 +0100

    responsive header

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,31 +1,15 @@
-const navigationLinks = document.querySelectorAll('li a')
-const changeBottleBtn = document.getElementById('change-bottle')
-const toggleInput = document.getElementById('toggle')
-const primaryNav = document.querySelector('.primary-navigation')
-const navToggle = document.querySelector('.mobile-nav-toggle')
+const primaryNav = document.getElementById('primary-navigation')
+const navToggle = document.querySelector('header button')
 
-// changeBottleBtn.addEventListener('click', e => {
-//     toggleInput.checked = !toggleInput.checked
-// })
-
-const currentPath = window.location.pathname;
-
-// console.log(currentPath)
-
-navigationLinks.forEach(link => {
-    if(link.getAttribute('href') === currentPath) {
-        link.classList.add('active')
-    }
-})
-
-// console.log(navToggle)
 navToggle.addEventListener('click', () => {
     const visibility = primaryNav.getAttribute('data-visible')
+    
     if (visibility === "false") {
-        primaryNav.setAttribute('data-visible', 'true')
+        primaryNav.setAttribute('data-visible', true)
         navToggle.setAttribute('aria-expanded', true)
     } else {
-        primaryNav.setAttribute('data-visible', 'false')
+        primaryNav.setAttribute('data-visible', false)
         navToggle.setAttribute('aria-expanded', false)
     }
+    console.log(navToggle.getAttribute('aria-expanded'))
 })
