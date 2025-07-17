29826480d0edb5543bdc55eb33152f5857000754
Kacperski81 <kacperski81@gmail.com>
2025-07-17 13:33:03 +0100

    main accordion

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,6 @@
 const primaryNav = document.getElementById('primary-navigation')
 const navToggle = document.querySelector('header button')
+const accordion = document.querySelector('.accordion')
 
 navToggle.addEventListener('click', () => {
     const visibility = primaryNav.getAttribute('data-visible')
@@ -13,3 +14,25 @@ navToggle.addEventListener('click', () => {
     }
     console.log(navToggle.getAttribute('aria-expanded'))
 })
+
+accordion.addEventListener('click', (e) => {
+    const activePanel = e.target.closest(".accordion-panel");
+    if (!activePanel) return;
+    toggleAccordion(activePanel)
+})
+
+function toggleAccordion(panelToActivate) {
+    const buttons = panelToActivate.parentElement.querySelectorAll("button");
+    const contents = panelToActivate.parentElement.querySelectorAll(".accordion-content");
+
+    buttons.forEach((button) => {
+        button.setAttribute("aria-expanded", false);
+    });
+
+    contents.forEach((content) => {
+        content.setAttribute("aria-hidden", true)
+    })
+
+    panelToActivate.querySelector("button").setAttribute("aria-expanded", true);
+    panelToActivate.querySelector(".accordion-content").setAttribute("aria-hidden", false);
+}
